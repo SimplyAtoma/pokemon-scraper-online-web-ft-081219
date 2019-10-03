8b2fe4a369f038ff9e3e9b4a7c8b15ec12d856d8
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-10-03 01:21:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.save(new_name, new_type, db)
+    db.execute("INSERT INTO pokemon(name,type) VALUES ('#{new_name}', '#{new_type}')")
+  end 
+  
+  def self.find(id, db)
+    db.execute("SELECT * FROM pokemon WHERE id = #{id}")
+    
+  end 
 end
